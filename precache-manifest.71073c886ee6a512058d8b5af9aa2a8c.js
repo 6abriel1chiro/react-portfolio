96651eecb010364150b8211c31c654d1f4c94dc0
Gabriel Ichiro <gabriel582084@gmail.com>
2023-06-02 01:18:23 -0400

    Deploy React app to GitHub Pages

---
+++ precache-manifest.71073c886ee6a512058d8b5af9aa2a8c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "712006b6e60451a5f43b",
+    "url": "/react-portfolio/static/css/main.26f2cb39.chunk.css"
+  },
+  {
+    "revision": "712006b6e60451a5f43b",
+    "url": "/react-portfolio/static/js/main.848c0f73.chunk.js"
+  },
+  {
+    "revision": "c39b5de948bed8c395fd",
+    "url": "/react-portfolio/static/js/runtime~main.b9a206e0.js"
+  },
+  {
+    "revision": "b33297ae74a0cb784c31",
+    "url": "/react-portfolio/static/css/2.f16d378d.chunk.css"
+  },
+  {
+    "revision": "b33297ae74a0cb784c31",
+    "url": "/react-portfolio/static/js/2.fe1d6ca6.chunk.js"
+  },
+  {
+    "revision": "78c46219922c2670ff5a",
+    "url": "/react-portfolio/static/js/3.b413865b.chunk.js"
+  },
+  {
+    "revision": "d0bf35a6fb69a0aa8bd8f8568721e67b",
+    "url": "/react-portfolio/static/media/microservice.d0bf35a6.jpeg"
+  },
+  {
+    "revision": "26a476d3e7d029ba7e45903d1d80ca82",
+    "url": "/react-portfolio/static/media/flask_todo.26a476d3.jpeg"
+  },
+  {
+    "revision": "e7c93467f126196a92a7c24ac988c7f0",
+    "url": "/react-portfolio/static/media/teeko.e7c93467.jpeg"
+  },
+  {
+    "revision": "de5be14fe537440a0f16accd93d6b4bd",
+    "url": "/react-portfolio/static/media/angular.de5be14f.jpeg"
+  },
+  {
+    "revision": "359dfe12f8980f55eac52b3f48865f79",
+    "url": "/react-portfolio/static/media/react.359dfe12.jpeg"
+  },
+  {
+    "revision": "7b00f74f5aabbaa9d563fd7534371d37",
+    "url": "/react-portfolio/index.html"
+  }
+];
