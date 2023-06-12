202dc5953b33f975767f02761db4e790b92a00ea
Gabriel Ichiro <gabriel582084@gmail.com>
2023-06-12 17:53:05 -0400

    Updates

---
+++ precache-manifest.273e4629e0a4d9b5601164262234954c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "52efde6d89c60462432c",
+    "url": "/react-portfolio/static/css/main.50ef5f2d.chunk.css"
+  },
+  {
+    "revision": "52efde6d89c60462432c",
+    "url": "/react-portfolio/static/js/main.5b0ac197.chunk.js"
+  },
+  {
+    "revision": "c39b5de948bed8c395fd",
+    "url": "/react-portfolio/static/js/runtime~main.b9a206e0.js"
+  },
+  {
+    "revision": "9ee84b4b664c70534778",
+    "url": "/react-portfolio/static/css/2.f16d378d.chunk.css"
+  },
+  {
+    "revision": "9ee84b4b664c70534778",
+    "url": "/react-portfolio/static/js/2.1bf5af76.chunk.js"
+  },
+  {
+    "revision": "78c46219922c2670ff5a",
+    "url": "/react-portfolio/static/js/3.b413865b.chunk.js"
+  },
+  {
+    "revision": "d0bf35a6fb69a0aa8bd8f8568721e67b",
+    "url": "/react-portfolio/static/media/microservice.d0bf35a6.jpeg"
+  },
+  {
+    "revision": "26a476d3e7d029ba7e45903d1d80ca82",
+    "url": "/react-portfolio/static/media/flask_todo.26a476d3.jpeg"
+  },
+  {
+    "revision": "e7c93467f126196a92a7c24ac988c7f0",
+    "url": "/react-portfolio/static/media/teeko.e7c93467.jpeg"
+  },
+  {
+    "revision": "de5be14fe537440a0f16accd93d6b4bd",
+    "url": "/react-portfolio/static/media/angular.de5be14f.jpeg"
+  },
+  {
+    "revision": "359dfe12f8980f55eac52b3f48865f79",
+    "url": "/react-portfolio/static/media/react.359dfe12.jpeg"
+  },
+  {
+    "revision": "b97b92489ccc5fbca787113b2999d45d",
+    "url": "/react-portfolio/index.html"
+  }
+];
